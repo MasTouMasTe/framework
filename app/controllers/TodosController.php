0cf29430d6faee2ab0b444694504e5675f1922d0
unknown <clementgheist68@gmail.com>
2021-01-28 10:49:15 +0100

    routes

---
+++ app/controllers/TodosController.php
@@ -0,0 +1,63 @@
+<?php
+namespace controllers;
+use Ubiquity\attributes\items\router\Route;
+
+ /**
+ * Controller TodosController
+ **/
+class TodosController extends ControllerBase{
+
+    const CACHE_KEY = 'datas/lists/';
+    const EMPTY_LIST_ID='not saved';
+    const LIST_SESSION_KEY='list';
+    const ACTIVE_LIST_SESSION_KEY='active-list';
+
+	#[Route(path: "todos/delete/{index}", name:'todos.delete', methods:['get'])]
+	    public function deleteElement($index){
+		
+	    }
+
+    #[Route(path: "todos/add", name:'todos.add', methods:['post'])]
+    	public function addElement(){
+
+    	}
+
+    #[Route(path: "todos/edit/{index}", name:'todos.edit', methods:['post'])]
+    	public function editElement($index){
+
+    	}
+
+    #[Route(path: "todos/loadList/{index}", name:'todos.loadList', methods:['get'])]
+    	public function loadList($uniqid){
+
+    }
+
+    #[Route(path: "todos/loadList", name:'todos.loadListPost', methods:['post'])]
+    	public function loadListFromForm(){
+
+    }
+
+    #[Route(path: "todos/new/{index}", name:'todos.new', methods:['get'])]
+    	public function newlist($force){
+
+    	}
+
+    #[Route(path: "todos/saveList", name:'todos.save', methods:['get'])]
+    	public function saveList(){
+
+    	}
+
+    #[Route(path: "default", name:'home')]
+        	public function index(){
+
+        }
+
+    private function menu(){
+
+        $this->loadView(viewName: 'TodosController/menu.html');
+
+
+    }
+
+
+}
