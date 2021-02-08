cbd01763899ee540ff682ad75d5bc0ca4ded3671
unknown <clementgheist68@gmail.com>
2021-02-08 13:02:44 +0100

    Fin de séance + toString

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -6,20 +6,24 @@ namespace controllers;
  /**
   * Controller OrgaController
   */
-class OrgaController extends ControllerBase{
+ class OrgaController extends ControllerBase{
+     #[Route('orga')]
+     public function index(){
+         //$orgas=DAO::getAll(Organization::class, "like '%uni%'",true);
+         //$this->loadView("OrgaController/index.html",['orga'=>$orgas]);
+     }
 
-    #[Route(path: "orga",name: "orga.index2")]
-	public function index(){
-		$orgas=DAO::getAll(  Organization::class,  "", "name like '%uni%'", ['groupes.users']);
-		var_dump($orgas);
-		$this->loadView( "OrgaController/index.html", ['orgas'=> $orgas]);
-	}
+     #[Route(path: "orga",name: "orga.index2")]
+     public function index2(){
+         $orgas=DAO::getAll(Organization::class, "",false);
+         $this->loadView("OrgaController/index.html",['orgas'=>$orgas]);
+     }
 
-	
-	public function getOne($idOrga){
-		
-		$this->loadView('OrgaController/getOne.html');
 
-	}
+     #[Route(path: "orga/{idOrga}",name: "orga.getOne")]
+     public function getOne($idOrga){
+         $orga=DAO::getById(Organization::class, $idOrga,['groupe.users']);
+         $this->loadDefaultView(['orga'=>$orga]);
+     }
 
-}
+ }
