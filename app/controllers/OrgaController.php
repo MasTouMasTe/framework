c2daaa90a4ef06b9cd36ebeb72cbc694e78b8e3e
moi <clementgheist68@gmail.com>
2021-02-15 08:29:32 +0100

    update + insert

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -1,7 +1,9 @@
 <?php
 namespace controllers;
  use models\Organization;
+ use models\User;
  use Ubiquity\orm\DAO;
+ use Ubiquity\utils\http\URequest;
 
  /**
   * Controller OrgaController
@@ -24,6 +26,34 @@ namespace controllers;
      public function getOne($idOrga){
          $orga=DAO::getById(Organization::class, $idOrga,['groupe.users']);
          $this->loadDefaultView(['orga'=>$orga]);
+     }
+
+     #[Route()]
+     public function getOrga($name, $aliases){
+        $orga = DAO::getOne(Organization::classe,"name= ? and aliases= ?",false, parameters:[$name, $aliases]);
+     }
+
+     public function testInsert(){
+         $groupe = new Groupe();
+         URequest::setValuesToObject($groupe);
+         $idOrga=URequest::post('idOrga');
+         $orga=DAO::getById(Organization::class,$idOrga);
+         $groupe->setOrganization($orga);
+         DAO::insert($orga);
+     }
+
+     public function testUpdate(){
+         $groupe=DAO::getById(Groupe::class,URequest::post('idGroupe'));
+         URequest::setValuesToObject($groupe);
+         $idOrga=URequest::post('idOrga');
+         $orga=DAO::getById(Organization::class,$idOrga);
+         $groupe->setOrganization($orga);
+         $idUsers=explode(',',URequest::post('idUsers'));
+         $users=DAO::getAllByIds(User::class,$idUsers);
+         foreach($users as $user){
+             $groupe->addUsers($user);
+         }
+         DAO::update($orga, true);
      }
 
  }
