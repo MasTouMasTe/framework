6adfe0ce1c7f0108123928bf1c83e1c624941293
moi <clementgheist68@gmail.com>
2021-03-08 15:51:02 +0100

    Modification controller MyAuth

--- app/controllers/MyAuth.php
+++ app/controllers/MyAuth.php
@@ -8,4 +8,24 @@ class MyAuth extends ControllerBase{
 	public function index(){
 		$this->loadView("MyAuth/index.html");
 	}
+
+    public function _displayInfoAsString() {
+        return true;
+    }
+
+    protected function finalizeAuth() {
+        if(!URequest::isAjax()){
+            $this->loadView('@activeTheme/main/vFooter.html');
+        }
+    }
+
+    protected function initializeAuth() {
+        if(!URequest::isAjax()){
+            $this->loadView('@activeTheme/main/vHeader.html');
+        }
+    }
+
+    public function _getBodySelector() {
+        return '#page-container';
+    }
 }
